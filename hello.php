21eec511612d8261d2fe313a24dc45505515ac58
Razvan <razvan@acad.ropardo.ro>
2024-03-13 00:45:26 -0700

    Section 3

--- hello.php
+++ hello.php
@@ -8,9 +8,27 @@ echo $message;
 var_dump($message);
 var_dump($count);
 var_dump($price);
-*/
 
 $is_admin=true;
 $data=null;
 
 var_dump($is_admin,$data);
+
+$count =10;
+$size=2;
+$price=2.95;
+$quantity=5;
+
+var_dump($count+5);
+var_dump($count * $size);
+var_dump($price*$quantity);*/
+
+$message='Hello';
+$name="Bogdan";
+
+$start="3 o'clock";
+$end='4 o\'clock';
+
+$days="Monday\nThuesday\nWednesday";
+
+echo "Hello {$name}";
