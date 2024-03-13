2b99f39a042dab57e9ae96aee371dfd1b7a2709c
Razvan <razvan@acad.ropardo.ro>
2024-03-13 00:23:41 -0700

    first commit

---
+++ hello.php
@@ -0,0 +1,16 @@
+<?php
+/*
+$message="Hello Again!";
+$count =3;
+$price=1.99;
+
+echo $message;
+var_dump($message);
+var_dump($count);
+var_dump($price);
+*/
+
+$is_admin=true;
+$data=null;
+
+var_dump($is_admin,$data);
