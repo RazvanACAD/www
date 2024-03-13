3e512d2b3c246ea17144128cf5ae6d8336b82474
Razvan <razvan@acad.ropardo.ro>
2024-03-13 02:13:42 -0700

    S6/27

---
+++ index.php
@@ -0,0 +1,9 @@
+<h1>Lorem Ipsum</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Cum et delectus harum qui excepturi voluptatum animi quibusdam reiciendis eaque 
+possimus numquam, corporis quaerat, corrupti, eveniet facilis molestias ad mollitia atque.</p>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+<i>Cum</i> et delectus harum qui excepturi voluptatum animi quibusdam reiciendis eaque 
+possimus numquam, corporis quaerat, corrupti, eveniet facilis molestias ad mollitia atque.</p>
