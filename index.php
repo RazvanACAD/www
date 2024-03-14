72f3a651ee58453d2b61b69de9bdec1961e54533
Razvan <razvan@acad.ropardo.ro>
2024-03-14 00:32:23 -0700

    S7/39

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
-<h1>Lorem Ipsum</h1>
+<?php
+$name = "Dave";
+$hour =11
+?>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-Cum et delectus harum qui excepturi voluptatum animi quibusdam reiciendis eaque 
-possimus numquam, corporis quaerat, corrupti, eveniet facilis molestias ad mollitia atque.</p>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My website</title>
+  </head>
+  <body>
+    <h1>Lorem Ipsum</h1>
+    <p>Hello <?= $name;?>!</p>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-<i>Cum</i> et delectus harum qui excepturi voluptatum animi quibusdam reiciendis eaque 
-possimus numquam, corporis quaerat, corrupti, eveniet facilis molestias ad mollitia atque.</p>
+    <?php if ($hour < 12): ?>
+      Good morning!
+    <?php elseif($hour < 18): ?>
+      Good afternoon!
+    <?php elseif($hour<22): ?>
+      Good evening!
+    <?php else: ?>
+      Good night!
+    <?php endif; ?>
+  </body>
+</html>
+
+
