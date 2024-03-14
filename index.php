f63ee6bcc7d0075d1fb65f4b80d22d988e606f93
Razvan <razvan@acad.ropardo.ro>
2024-03-14 01:39:05 -0700

    S8/Database

--- index.php
+++ index.php
@@ -1,6 +1,52 @@
 <?php
+
+$db_host = "localhost";
+$db_name = "cms";
+$db_user = "cms_www";
+$db_pass = "YMuhbcJRdjj*Ak21";
+
+$conn = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+
+if(mysqli_connect_error()){
+  echo mysqli_connect_error();
+  exit;
+}
+
+//echo "Connected succesfully";
+
+$sql = "SELECT * FROM article  ORDER BY id;";
+
+$results = mysqli_query($conn,$sql);
+
+if($results===false){
+  echo mysqli_error($conn);
+}else{
+  $articles= mysqli_fetch_all($results,MYSQLI_ASSOC);
+
+}
+
+/*
 $name = "Dave";
-$hour =11
+$hour =11;
+
+$articles = [
+  [
+    "title"=>"First Post",
+    "content"=>"This is the first post"
+  ],
+  [
+    "title"=>"Another Post",
+    "content"=>"This is another post"
+  ],
+  [
+    "title"=>"Read This!",
+    "content"=>"YOU MUST READ THIS NOW!!!!"
+  ],
+  [
+    "title"=>"Hello!",
+    "content"=>"READ THIS NOW!!!!"
+  ]
+]*/
 ?>
 
 <!DOCTYPE html>
@@ -11,18 +57,26 @@ $hour =11
     <title>My website</title>
   </head>
   <body>
-    <h1>Lorem Ipsum</h1>
-    <p>Hello <?= $name;?>!</p>
+    <header>
+      <h1>My Blog</h1>
+    </header>
 
-    <?php if ($hour < 12): ?>
-      Good morning!
-    <?php elseif($hour < 18): ?>
-      Good afternoon!
-    <?php elseif($hour<22): ?>
-      Good evening!
-    <?php else: ?>
-      Good night!
-    <?php endif; ?>
+    <main>
+      <?php if(empty($articles)): ?>
+        <p>No articles found</p>
+      <?php else: ?>
+      <ul>
+        <?php foreach ($articles as $article): ?>
+          <li>
+            <article>
+              <h2><?= $article['title'];?></h2>
+              <p><?= $article['content'];?></p>
+            </article>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+      <?php endif; ?>
+    </main>
   </body>
 </html>
 
