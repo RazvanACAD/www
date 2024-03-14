cd5e5cb54758b2b1ff71451a1708a4c3aee7e47f
Razvan <razvan@acad.ropardo.ro>
2024-03-14 01:52:49 -0700

    S9/48 New Page to show Single Article

---
+++ article.php
@@ -0,0 +1,59 @@
+<?php
+
+$db_host = "localhost";
+$db_name = "cms";
+$db_user = "cms_www";
+$db_pass = "YMuhbcJRdjj*Ak21";
+
+$conn = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+
+if(mysqli_connect_error()){
+  echo mysqli_connect_error();
+  exit;
+}
+
+$sql = "SELECT * FROM article WHERE id=1;";
+
+$results = mysqli_query($conn,$sql);
+
+if($results===false){
+  echo mysqli_error($conn);
+}else{
+  $article= mysqli_fetch_assoc($results);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My website</title>
+  </head>
+  <body>
+    <header>
+      <h1>My Blog</h1>
+    </header>
+
+    <main>
+      <?php if($article===null): ?>
+        <p>Article notfound</p>
+      <?php else: ?>
+      <ul>
+        
+          <li>
+            <article>
+              <h2><?= $article['title'];?></h2>
+              <p><?= $article['content'];?></p>
+            </article>
+          </li>
+      
+      </ul>
+      <?php endif; ?>
+    </main>
+  </body>
+</html>
+
+
+
