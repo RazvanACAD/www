3b0f4e55a2411ec7af126988bd8b6162c229f8de
Razvan <razvan@acad.ropardo.ro>
2024-03-21 04:54:33 -0700

    118

--- edit-article.php
+++ edit-article.php
@@ -2,7 +2,6 @@
 
 require 'classes/Database.php';
 require 'classes/Article.php';
-require 'includes/article.php';
 require 'includes/url.php';
 
 $db = new Database();
@@ -29,17 +28,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $article->title = $_POST['title'];
     $article->content = $_POST['content'];
     $article->published_at = $_POST['published_at'];
+ 
+    if ($article->update($conn)) {
 
-    $errors = validateArticle($article->title, $article->content, $article->published_at);
+        redirect("/article.php?id={$article->id}");
 
-    if (empty($errors)) {
-
-        if ($article->update($conn)) {
-
-            redirect("/article.php?id={$article->id}");
-
-        }
-        
     }
 }
 
