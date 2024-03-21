ae0f1efe34f25485277d7277f8291d7434073384
Razvan <razvan@acad.ropardo.ro>
2024-03-21 03:17:46 -0700

    113

--- classes/Database.php
+++ classes/Database.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * Database
+ * 
+ * A connection to the database
+ */
 class Database
 {
   /**
@@ -16,6 +20,20 @@ class Database
 
     $dsn = 'mysql:host=' . $db_host . ';dbname=' . $db_name .';charset=utf8';
 
-    return new PDO($dsn,$db_user,$db_pass);
+    try{
+
+      $db = new PDO($dsn,$db_user,$db_pass);
+
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      return $db;
+
+    }catch(PDOException $e){
+
+      echo $e->getMessage();
+      exit;
+
+    }
+    
   }
 }
