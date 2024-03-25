1e8161201ae23d398e371d8fcfda34d182e4f4c3
Razvan <razvan@acad.ropardo.ro>
2024-03-25 10:34:08 +0200

    Section 24: Using relationships: Article - Categories; 167: Insert Article Categories while Avoiding Duplicate Records in the Join Table

---
+++ classes/Category.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Category
+ * 
+ * Groupings for article
+ */
+class Category{
+
+    /**
+     * Get all the categories
+     *
+     * @param object $conn Connection to the database
+     *
+     * @return array An associative array of all the article records
+     */
+    public static function getAll($conn)
+    {
+        $sql = "SELECT *
+                FROM category
+                ORDER BY name;";
+
+        $results = $conn->query($sql);
+
+        return $results->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
